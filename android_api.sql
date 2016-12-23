2b7d9c66742741be7c76caa4d708c82496c443ef
khouloud93 <benkhssib.khouloud93@gmail.com>
2016-12-24 00:20:51 +0100

    Add files via upload

---
+++ android_api.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Sam 24 Décembre 2016 à 00:05
+-- Version du serveur :  10.1.19-MariaDB
+-- Version de PHP :  5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `android_api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `chapitre`
+--
+
+CREATE TABLE `chapitre` (
+  `id_chap` int(11) NOT NULL,
+  `titre_chap` varchar(20) NOT NULL,
+  `description_chap` varchar(20) DEFAULT NULL,
+  `id_cour` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cour`
+--
+
+CREATE TABLE `cour` (
+  `id_cour` int(11) NOT NULL,
+  `libelle_cour` varchar(20) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cour_user`
+--
+
+CREATE TABLE `cour_user` (
+  `id_cour` int(11) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lecon`
+--
+
+CREATE TABLE `lecon` (
+  `id_lec` int(11) NOT NULL,
+  `titre_lec` varchar(20) NOT NULL,
+  `url_video_lec` varchar(500) NOT NULL,
+  `description_lec` varchar(2000) NOT NULL,
+  `id_chap` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `id_quiz` int(11) NOT NULL,
+  `question` varchar(60) NOT NULL,
+  `rep` varchar(20) NOT NULL,
+  `choix_a` varchar(20) NOT NULL,
+  `choix_b` varchar(20) NOT NULL,
+  `choix_c` varchar(20) NOT NULL,
+  `id_lec` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `unique_id` varchar(23) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `encrypted_password` varchar(80) NOT NULL,
+  `profil` varchar(20) NOT NULL,
+  `salt` varchar(10) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `unique_id`, `name`, `email`, `encrypted_password`, `profil`, `salt`, `created_at`, `updated_at`) VALUES
+(1, '1', 'khouloud', 'benkhssib.khouloud93@gmail.com', '48789789798777878878', '', '', NULL, NULL);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `chapitre`
+--
+ALTER TABLE `chapitre`
+  ADD PRIMARY KEY (`id_chap`),
+  ADD UNIQUE KEY `id_cour_ref` (`id_cour`);
+
+--
+-- Index pour la table `cour`
+--
+ALTER TABLE `cour`
+  ADD PRIMARY KEY (`id_cour`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `cour_user`
+--
+ALTER TABLE `cour_user`
+  ADD PRIMARY KEY (`id_cour`,`id`),
+  ADD UNIQUE KEY `id_cour` (`id_cour`),
+  ADD UNIQUE KEY `id_user` (`id`);
+
+--
+-- Index pour la table `lecon`
+--
+ALTER TABLE `lecon`
+  ADD PRIMARY KEY (`id_lec`),
+  ADD UNIQUE KEY `id_chap` (`id_chap`);
+
+--
+-- Index pour la table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`id_quiz`),
+  ADD UNIQUE KEY `id_lec` (`id_lec`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_id` (`unique_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `cour`
+--
+ALTER TABLE `cour`
+  MODIFY `id_cour` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `lecon`
+--
+ALTER TABLE `lecon`
+  MODIFY `id_lec` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `id_quiz` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `chapitre`
+--
+ALTER TABLE `chapitre`
+  ADD CONSTRAINT `chapitre_ibfk_1` FOREIGN KEY (`id_cour`) REFERENCES `cour` (`id_cour`);
+
+--
+-- Contraintes pour la table `cour`
+--
+ALTER TABLE `cour`
+  ADD CONSTRAINT `cour_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `cour_user`
+--
+ALTER TABLE `cour_user`
+  ADD CONSTRAINT `cour_user_ibfk_1` FOREIGN KEY (`id_cour`) REFERENCES `cour` (`id_cour`),
+  ADD CONSTRAINT `cour_user_ibfk_2` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `lecon`
+--
+ALTER TABLE `lecon`
+  ADD CONSTRAINT `lecon_ibfk_1` FOREIGN KEY (`id_chap`) REFERENCES `chapitre` (`id_chap`);
+
+--
+-- Contraintes pour la table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD CONSTRAINT `quiz_ibfk_1` FOREIGN KEY (`id_lec`) REFERENCES `lecon` (`id_lec`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
